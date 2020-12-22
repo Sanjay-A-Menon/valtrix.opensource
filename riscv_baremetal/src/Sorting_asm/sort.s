f466d3b214d52138d705104f55aa36330c6bf144
Sanjay Menon <smenon@valtrix.in>
2020-12-22 11:27:12 +0530

    Added folder Sorting_asm with code

--- riscv_baremetal/src/Sorting_asm/sort.s
+++ riscv_baremetal/src/Sorting_asm/sort.s
@@ -2,31 +2,28 @@
 
     .global _start
 _start:
-    //li  x10, 1
-   // li  x11, 33
-   // add x10, x10, x11
-
 
 load_arr:
-    la x5, arr
-
+    la x5, arr        //Initialising a pointer to the array.
     addi x5,x5,-8
 
-outer_loop:
-    addi x5,x5,8
+
+//Loop to determine the initial data, which is then compared with the rest.
+outer_loop:            
+    addi x5,x5,8       
     addi x13,x5,8
     ld x6,0(x5)
     beq x6, zero, hang
-    //addi x5,x5,8
 
-inner_loop:
+//Loop to move through the rest of the array after the initial data point.
+inner_loop:         
     ld x7, 0(x13)
     beq x7, zero, outer_loop
     bgt  x6, x7, lbl_gt
     addi x13,x13,8
     j inner_loop
 
-
+//Determining which value is smaller/larger and swapping places accordingly.
  lbl_gt:
         addi x8, x6, 0
         addi x6, x7, 0
@@ -36,28 +33,15 @@ inner_loop:
         addi x13,x13,8
         j inner_loop
 
-        
- //lbl_lt:
-
-  //  j lo
-
-
-//my_func:
-//dfg
-//sdfg
-
-
-//    ret
-
-
-
-
+//A simple loop to hang
 hang:
     j hang
 
 
     .data
     .balign 8       // else we fault
+
+//The array to be sorted.
 arr:
     .dword 67, 56, 32, 86, 99, 3, 12, 19, 33, 42, 0
 
