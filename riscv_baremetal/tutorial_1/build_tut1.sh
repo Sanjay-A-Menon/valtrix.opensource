e6f0ab1969937b002fb764f14b1c8affe8680a9b
Sanjay Menon <smenon@valtrix.in>
2021-01-16 00:14:28 +0530

    Build file comments added

--- riscv_baremetal/tutorial_1/build_tut1.sh
+++ riscv_baremetal/tutorial_1/build_tut1.sh
@@ -8,3 +8,16 @@ riscv64-unknown-elf-objdump -xsD ${IMG_NAME}.elf > ${IMG_NAME}.dis
 
 set +o xtrace
 
+########  Notes/Explanation   #########
+###    Explanation  ####
+#
+# 1. The command on line 4 is used to build assembly files(denoted by .s extension) to object files(denoted by .o extension).
+# 2. The command on Line 5 is used to build C++ files(denoted by .cpp extension) to object files.
+# 3. Line 6 takes all object files thus produced and Linkes them together based on a Linker script provided(here linker_tut1.script) and produces a output map and an executable of .elf format.
+# 4. The statement on Line 7 is to dump the .elf file to a disassembly file, which is not required by the simulator but is used for debugging purposes by the programmer.
+#
+###    Notes   ###
+# 
+# 1. A disassembly file shows the way the RISCV core executes all of the instructions written along with their corresponding code memory address.
+#
+######## end ##########
